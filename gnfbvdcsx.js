375bb056e743efb992cf8fd658c610a74a9cb2fe
GregOliv <136775038+GregOliv@users.noreply.github.com>
2023-09-17 19:54:19 +0700

    Update gnfbvdcsx.js

--- gnfbvdcsx.js
+++ gnfbvdcsx.js
@@ -1,1 +1,8 @@
-window.fuckadbDetector404 = true
+window.onload = function() {
+    setTimeout(function() {
+        var ad = document.querySelector("ins.adsbygoogle");
+        if (ad && ad.innerHTML.replace(/\s/g, "").length == 0) {
+            console.log('You seem to blocking Google AdSense ads in your browser.');
+        }
+    }, 2000);
+};
